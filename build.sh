8b25eb45c61390b68fd73bfae55c73054d8dd837
Johannes Bensmann <johannes.bensmann@gmx.de>
2022-06-25 16:43:56 +0200

    create a binary for every architecture

--- build.sh
+++ build.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 targets="linux,amd64"
+
+rm -rf dist/
+
 for i in $targets; do
     IFS=","
     set -- $i
     os=$1
     arch=$2
     echo "building $os:$arch"
-    GOOS=$os GOARCH=$arch go build -ldflags="-extldflags=-static" -o dist/${os}_${arch}/ .
+    GOOS=$os GOARCH=$arch go build -ldflags="-extldflags=-static" -o dist/mouseless .
     if [ $? != 0 ]; then
         exit 1
     fi
+    tar -czvf dist/mouseless-${os}-${arch}.tar.gz dist/mouseless
 done
 
-rm -f mouseless.zip
-zip -r mouseless.zip dist
 
