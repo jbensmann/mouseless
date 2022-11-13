3ec63eafd09868eaa20a5fc29a8d4b1f462bcbe3
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2022-11-13 14:49:19 +0000

    Add auto-installation script

---
+++ install.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+version=0.1.3
+# if the 1st command fails, the second will activate.
+keyboard=$(ls /dev/input/by-id/*kbd* || ls /dev/input/by-path/*kbd*)
+# nv means no verbose
+wget -P /home/$USER/ https://github.com/jbensmann/mouseless/releases/download/v$version/mouseless-linux-amd64.tar.gz
+tar -xf /home/$USER/mouseless-linux-amd64.tar.gz --directory /home/$USER/
+sudo cp /home/$USER/dist/mouseless /bin/
+# copies the default config to the .config directory
+mkdir /home/$USER/.config/mouseless
+touch /home/$USER/.config/mouseless/config.yaml 
+echo "
+devices:
+# change this to a keyboard device
+- "$keyboard"
+# this is executed when mouseless starts
+# startCommand: ""
+# the default speed for mouse movement
+baseMouseSpeed: 750.0
+# the default speed for scrolling
+baseScrollSpeed: 20.0
+layers:
+# the first layer is active at start
+- name: initial
+  bindings:
+    # when tab is held and another key pressed, activate mouse layer
+    tab: tap-hold-next tab ; toggle-layer mouse ; 500
+    # when a is held for 300ms, activate mouse layer
+    a: tap-hold a ; toggle-layer mouse ; 300
+    # right alt key toggles arrows layer
+    rightalt: toggle-layer arrows
+    # switch escape with capslock
+    esc: capslock
+    capslock: esc
+# a layer for mouse movement
+- name: mouse
+  # when true, keys that are not mapped keep their original meaning
+  passThrough: true
+  bindings:
+    # quit mouse layer
+    q: layer initial
+    # keep the mouse layer active
+    space: layer mouse
+    r: reload-config
+    l: move  1  0
+    j: move -1  0
+    k: move  0  1
+    i: move  0 -1
+    p: scroll up
+    n: scroll down
+    leftalt: speed 4.0
+    e: speed 0.3
+    capslock: speed 0.1
+    f: button left
+    d: button middle
+    s: button right
+    # move to the top left corner
+    k0: "exec xdotool mousemove 0 0"
+# another layer for arrows and some other keys
+- name: arrows
+  passThrough: true
+  bindings:
+    e: up
+    s: left
+    d: down
+    f: right
+    q: esc
+    w: backspace
+    r: delete
+    v: enter" > /home/$USER/.config/mouseless/config.yaml 
+# Creates an entry that automatically launches mouseless with the --config flag
+touch mouseless-execute
+echo "
+#!/bin/sh 
+sudo mouseless --config ~/.config/mouseless/config.yaml" > /home/$USER/mouseless-execute
+chmod +x /home/$USER/mouseless-execute
+sudo cp /home/$USER/mouseless-execute /bin/
+# Removes the downloaded files
+rm /home/$USER/mouseless-execute /home/$USER/mouseless-linux-amd64.tar.gz
+rm -r /home/$USER/dist
+echo "Installation complete. Type mouseless-execute to launch the app alongside the config file. If you want to use other flags, use mouseless. If you wish do delete these entries, type sudo rm /bin/mouseless /bin/mouseless-execute."
