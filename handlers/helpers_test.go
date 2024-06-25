c874279484383b3fcb59cc57d166004d87d95200
Johannes Bensmann <johannes.bensmann@gmx.de>
2024-06-25 20:43:47 +0200

    test: added some tests for TapHoldHandler

---
+++ handlers/helpers_test.go
@@ -0,0 +1,178 @@
+package handlers
+
+import (
+	"fmt"
+	"github.com/jbensmann/mouseless/config"
+	"github.com/jbensmann/mouseless/keyboard"
+	"reflect"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+)
+
+type EventHandlerMock struct {
+	layers       []*config.Layer
+	currentLayer string
+
+	toggleLayerKeys     []uint16
+	toggleLayerPrevious []string
+
+	eventBindings []EventBinding
+}
+
+func NewEventHandlerMock(conf *config.Config) *EventHandlerMock {
+	return &EventHandlerMock{
+		layers:       conf.Layers,
+		currentLayer: "1",
+	}
+}
+
+func (b *EventHandlerMock) HandleEvent(eventBinding EventBinding) {
+	b.eventBindings = append(b.eventBindings, eventBinding)
+	event := eventBinding.Event
+	binding := eventBinding.Binding
+
+	if event.IsPress {
+		// check for toggle-layer binding
+		if binding, ok := binding.(config.ToggleLayerBinding); ok {
+			b.currentLayer = binding.Layer
+			b.toggleLayerKeys = append(b.toggleLayerKeys, event.Code)
+			b.toggleLayerPrevious = append(b.toggleLayerPrevious, b.currentLayer)
+		}
+	} else {
+		// go back to the previous layer when toggleLayerKey is released
+		for i, key := range b.toggleLayerKeys {
+			if key == event.Code {
+				b.currentLayer = b.toggleLayerPrevious[i]
+				// all layers that have been toggled after the current one are removed as well
+				b.toggleLayerKeys = b.toggleLayerKeys[:i]
+				b.toggleLayerPrevious = b.toggleLayerPrevious[:i]
+				return
+			}
+		}
+	}
+}
+
+func (b *EventHandlerMock) BaseLayer() *config.Layer {
+	return b.layers[0]
+}
+
+func (b *EventHandlerMock) CurrentLayer() *config.Layer {
+	for _, layer := range b.layers {
+		if layer.Name == b.currentLayer {
+			return layer
+		}
+	}
+	panic(fmt.Sprintf("non existing layer: %s", b.currentLayer))
+}
+
+func (b *EventHandlerMock) SetNextHandler(_ EventHandler) {
+}
+
+func (b *EventHandlerMock) SetLayerManager(_ LayerManager) {
+}
+
+func testConfig(t *testing.T, configStr string, tests [][]string) {
+	conf, err := config.ParseConfig([]byte(configStr))
+	if err != nil {
+		t.Fatal(fmt.Sprintf("Error parsing config: %v", err))
+	}
+	for _, test := range tests {
+		testCase(t, conf, test[0], test[1])
+	}
+}
+
+func testCase(t *testing.T, conf *config.Config, events string, expectedEventBindings string) {
+	handlerMock := NewEventHandlerMock(conf)
+
+	tapHoldHandler := NewTapHoldHandler(int64(50))
+	tapHoldHandler.SetLayerManager(handlerMock)
+	tapHoldHandler.SetNextHandler(handlerMock)
+
+	// feed events in
+	feedEventsIn(tapHoldHandler, events)
+
+	// parse the expected event bindings
+	var expEventBindings []EventBinding
+	for _, exp := range strings.Split(expectedEventBindings, " ") {
+		expEventBindings = append(expEventBindings, parseEventBinding(exp))
+	}
+
+	// check if we received the expected number of events
+	if len(expEventBindings) != len(handlerMock.eventBindings) {
+		t.Errorf(
+			"expected %d event bindings but got %d for test case (%s, %s)",
+			len(expEventBindings),
+			len(handlerMock.eventBindings),
+			events,
+			expectedEventBindings,
+		)
+		return
+	}
+
+	// check if events and bindings are the same (except the time)
+	for i, expEventBinding := range expEventBindings {
+		actEventBinding := handlerMock.eventBindings[i]
+		if actEventBinding.Event.Code != expEventBinding.Event.Code || actEventBinding.Event.IsPress != expEventBinding.Event.IsPress ||
+			!reflect.DeepEqual(actEventBinding.Binding, expEventBinding.Binding) {
+			t.Errorf(
+				"expected (%s,%+v) but got (%s,%+v) at index %d for test case (%s, %s)",
+				convertEventToString(expEventBinding.Event),
+				expEventBinding.Binding,
+				convertEventToString(actEventBinding.Event),
+				actEventBinding.Binding,
+				i,
+				events,
+				expectedEventBindings,
+			)
+		}
+	}
+}
+
+func convertEventToString(event keyboard.Event) string {
+	alias, _ := config.GetKeyAlias(event.Code)
+	prefix := "R"
+	if event.IsPress {
+		prefix = "P"
+	}
+	return fmt.Sprintf("%s%s", prefix, alias)
+}
+
+func feedEventsIn(handler EventHandler, events string) {
+	for _, s := range strings.Split(events, " ") {
+		if s[0] == 'P' || s[0] == 'R' {
+			eventBinding := parseEventBinding(s)
+			handler.HandleEvent(eventBinding)
+		} else {
+			ms, err := strconv.ParseUint(s, 10, 16)
+			if err != nil {
+				panic(fmt.Sprintf("failed to parse milliseconds: %s", s))
+			}
+			time.Sleep(time.Duration(ms) * time.Millisecond)
+		}
+	}
+}
+
+func parseEventBinding(eventBinding string) EventBinding {
+	split := strings.Split(eventBinding, ":")
+	code, _ := config.GetKeyCode(split[0][1:])
+	event := keyboard.Event{
+		Code:    code,
+		IsPress: split[0][0] == 'P',
+		Time:    time.Now(),
+	}
+	var binding config.Binding
+	if len(split) > 1 {
+		b := split[1]
+		if b[0] == 'K' {
+			code, _ = config.GetKeyCode(b[1:])
+			binding = config.KeyBinding{KeyCombo: []uint16{code}}
+		} else if b[0] == 'L' {
+			binding = config.ToggleLayerBinding{Layer: b[1:]}
+		} else {
+			panic(fmt.Sprintf("unexpected binding type %v", b[0]))
+		}
+	}
+	return EventBinding{Event: event, Binding: binding}
+}
---
+++ handlers/taphold_test.go
@@ -0,0 +1,100 @@
+package handlers
+
+import (
+	"testing"
+)
+
+func TestUnmapped(t *testing.T) {
+	configStr := `
+layers:
+- name: 1
+  bindings:
+    a: tap-hold a ; x ; 10
+`
+	tests := [][]string{
+		{"Pc Rc", "Pc Rc"}, // events not mapped to tap-hold should be passed through
+		{"Pd Pc Rd Rc", "Pd Pc Rd Rc"},
+		{"Pa:Km 15 Ra", "Pa:Km Ra"}, // event already mapped to a binding
+	}
+	testConfig(t, configStr, tests)
+}
+
+func TestTapHold(t *testing.T) {
+	configStr := `
+layers:
+- name: 1
+  bindings:
+    a: tap-hold a ; x ; 10
+    b: tap-hold b ; toggle-layer 2 ; 10
+    c: c
+- name: 2
+  bindings:
+    a: a
+    c: tap-hold c ; m ; 10
+    d: tap-hold d ; toggle-layer 3 ; 10
+- name: 3
+  bindings:
+    a: a
+`
+	tests := [][]string{
+		{"Pa Ra", "Pa:Ka Ra"}, // tap
+		{"Pa 5 Ra", "Pa:Ka Ra"},
+		{"Pa 15 Ra", "Pa:Kx Ra"}, // hold
+		{"Pa 15 Ra Pa Ra", "Pa:Kx Ra Pa:Ka Ra"},
+		{"Pa Pc Ra Rc", "Pa:Ka Pc Ra Rc"}, // tap
+		{"Pa Pc Rc Ra", "Pa:Ka Pc Rc Ra"},
+		{"Pa Pc Rc 15 Ra", "Pa:Kx Pc Rc Ra"},
+		{"Pc Pa Rc 15 Ra", "Pc Rc Pa:Kx Ra"}, // the event order is changed (to prevent c from being held until the tap-hold decision)
+		{"Pc Pa Rc Ra", "Pc Rc Pa:Ka Ra"},    // order changed as well
+		{"Pb 5 Rb", "Pb:Kb Rb"},              // tap-hold in combination with toggle-layer
+		{"Pb 15 Rb", "Pb:L2 Rb"},
+		{"Pb 15 Pa Ra Rb", "Pb:L2 Pa Ra Rb"}, // a is just passed through
+		{"Pb 15 Pa Rb Ra", "Pb:L2 Pa Rb Ra"},
+		{"Pb 15 Pc Rc Rb", "Pb:L2 Pc:Kc Rc Rb"},
+		{"Pb 7 Pc 7 Rc Rb", "Pb:L2 Pc:Kc Rc Rb"},
+		{"Pb 15 Pc 15 Rc Rb", "Pb:L2 Pc:Km Rc Rb"}, // two holds triggered
+		{"Pb 15 Pc 15 Rb Rc", "Pb:L2 Pc:Km Rb Rc"},
+		{"Pb Pc 15 Rc Rb", "Pb:L2 Pc:Km Rc Rb"},
+		{"Pb Pd 15 Rd Rb", "Pb:L2 Pd:L3 Rd Rb"}, // two toggle-layer
+	}
+	testConfig(t, configStr, tests)
+}
+
+func TestTapHoldNext(t *testing.T) {
+	configStr := `
+layers:
+- name: 1
+  bindings:
+    a: tap-hold-next a ; x ; 10
+    b: tap-hold b ; y ; 10
+    c: c
+`
+	tests := [][]string{
+		{"Pa Ra", "Pa:Ka Ra"}, // tap
+		{"Pa 5 Ra", "Pa:Ka Ra"},
+		{"Pa 15 Ra", "Pa:Kx Ra"}, // hold
+		{"Pa 15 Ra Pa Ra", "Pa:Kx Ra Pa:Ka Ra"},
+		{"Pa Pc Ra Rc", "Pa:Kx Pc Ra Rc"}, // hold since c is pressed before release of a
+		{"Pa Pc Rc Ra", "Pa:Kx Pc Rc Ra"},
+		{"Pa Pc Rc 15 Ra", "Pa:Kx Pc Rc Ra"},
+		{"Pc Pa Rc 15 Ra", "Pc Rc Pa:Kx Ra"}, // order changed as with tap-hold
+		{"Pc Pa Rc Ra", "Pc Rc Pa:Ka Ra"},    // tap
+		{"Pa Pb Ra Rb", "Pa:Kx Ra Pb:Kb Rb"}, // in combination with tap-hold
+		{"Pa Pb Rb Ra", "Pa:Kx Pb:Kb Rb Ra"},
+		{"Pb Pa Rb Ra", "Pb:Kb Rb Pa:Ka Ra"},
+		{"Pb Pa Ra Rb", "Pb:Kb Pa:Ka Ra Rb"},
+		{"Pa 15 Pb Ra Rb", "Pa:Kx Ra Pb:Kb Rb"}, // pauses at different positions
+		{"Pa 15 Pb Rb Ra", "Pa:Kx Pb:Kb Rb Ra"},
+		{"Pb 15 Pa Rb Ra", "Pb:Ky Rb Pa:Ka Ra"},
+		{"Pb 15 Pa Ra Rb", "Pb:Ky Pa:Ka Ra Rb"},
+		{"Pa Pb 15 Ra Rb", "Pa:Kx Pb:Ky Ra Rb"},
+		{"Pa Pb 15 Rb Ra", "Pa:Kx Pb:Ky Rb Ra"},
+		{"Pb Pa 15 Rb Ra", "Pb:Ky Pa:Kx Rb Ra"},
+		{"Pb Pa 15 Ra Rb", "Pb:Ky Pa:Kx Ra Rb"},
+		{"Pa 7 Pb 7 Ra Rb", "Pa:Kx Ra Pb:Kb Rb"},
+		{"Pa 7 Pb 7 Rb Ra", "Pa:Kx Pb:Kb Rb Ra"},
+		{"Pb 7 Pa 7 Rb Ra", "Pb:Ky Rb Pa:Kx Ra"},
+		{"Pb 7 Pa 7 Ra Rb", "Pb:Ky Pa:Ka Ra Rb"},
+	}
+	testConfig(t, configStr, tests)
+}
